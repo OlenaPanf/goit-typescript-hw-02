8fab7e5d8d49f03d1fb700c70d06e03d34d60f3c
OlenaPanf <elena.el@ukr.net>
2024-06-28 21:29:20 +0300

    start

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "modern-normalize";
-import App from './components/App/App'
+import App from './components/App/App' //'./App.tsx'
 import './index.css'
 import ReactModal from 'react-modal';
 
 ReactModal.setAppElement('#root');
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+
