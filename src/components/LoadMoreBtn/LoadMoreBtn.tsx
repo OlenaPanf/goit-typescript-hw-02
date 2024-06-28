0c0ee15ea4bb62d0bd74a99c33adb8eac7c9ddc0
OlenaPanf <elena.el@ukr.net>
2024-06-29 00:33:51 +0300

    LoadMoreBtn.tsx

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,8 +1,19 @@
 import css from './LoadMoreBtn.module.css'
 
-export default function LoadMoreBtn({ onLoadMore }) {
+interface LoadMoreBtnProps {
+  onLoadMore: () => void; //бо функція
+}
+
+export default function LoadMoreBtn({ onLoadMore }: LoadMoreBtnProps) {
   return (
     <button className={css.button} onClick={onLoadMore}>Load more</button>
   );
 }
+
+//================Оригінал====================================
+// export default function LoadMoreBtn({ onLoadMore }) {
+//   return (
+//     <button className={css.button} onClick={onLoadMore}>Load more</button>
+//   );
+// }
 
