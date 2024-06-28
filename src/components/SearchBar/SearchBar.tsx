253f6a0208b2154b6512b4104cfde4b13896b5e0
OlenaPanf <elena.el@ukr.net>
2024-06-29 02:45:46 +0300

    SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,40 +1,51 @@
-//import React, { FormEvent } from 'react'; //бо onSubmit і preventDefault
+import { FormEvent } from 'react'; //бо onSubmit і preventDefault
 import css from './SearchBar.module.css'
 import { toast } from 'react-hot-toast';
 
+interface SearchBarProps {
+  onSearch: (keyword: string) => void;
+}
 
-//=============Оригінал=====================================
-export const SearchBar = ({ onSearch }) => {
-  
-	const handleSubmit = (event) => {
+export const SearchBar = ({ onSearch }: SearchBarProps) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const form = event.target;
-    const keyword = form.elements.keyword.value;
-        
-    // Якщо текстове поле порожнє, виводимо повідомлення і припиняємо виконання функції.
-    if(keyword.trim() === "") {
-        toast.error("Please enter search term!");
-        return;
+    const form = event.target as HTMLFormElement;
+    const keyword = (form.elements.namedItem('keyword') as HTMLInputElement).value;
+
+    if (keyword.trim() === "") {
+      toast.error("Please enter search term!");
+      return;
     }
-    
-    // У протилежному випадку викликаємо пропс і передаємо йому значення поля
+
     onSearch(keyword);
     form.reset();
-  };
+   };
 
   return (
-        <header className={css.header}>
-            <form className={css.form} onSubmit={handleSubmit}>
-              <input
-                  className={css.input}
-                    type="text"
-                    name="keyword"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                />
-                <button className={css.button} type="submit">Search</button>
-            </form>
-        </header>
-    );
+    <header className={css.header}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <input
+          className={css.input}
+          type="text"
+          name="keyword"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button className={css.button} type="submit">Search</button>
+      </form>
+    </header>
+  );
 };
+
+
+//=============Оригінал=====================================
+// export const SearchBar = ({ onSearch }) => {
+  
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+//         const form = event.target;
+//         const keyword = form.elements.keyword.value;
+        
+//     };
+// }
