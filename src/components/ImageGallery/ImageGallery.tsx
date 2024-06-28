c9852150ca8998fee702713155e6e8a144c06d10
OlenaPanf <elena.el@ukr.net>
2024-06-29 00:26:46 +0300

    ImageGallery.tsx

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,19 @@
 import css from './ImageGallery.module.css'
 import ImageCard from "../ImageCard/ImageCard"
 
-export default function ImageGallery({ cards, onCardClick }) {
+interface Image {
+  id: string;
+  urls: {
+    small: string;
+  };
+}
+
+interface ImageGalleryProps {
+  cards: Image[]; //масив карток
+  onCardClick: (id: string) => void; //бо функція
+}
+
+export default function ImageGallery({ cards, onCardClick }: ImageGalleryProps) {
   return (
     <ul className={css.gallery}>
       {cards.map((card) => (
@@ -12,5 +24,18 @@ export default function ImageGallery({ cards, onCardClick }) {
     </ul>
   );
 }
+
+//================Оригінал====================================
+// export default function ImageGallery({ cards, onCardClick }) {
+//   return (
+//     <ul className={css.gallery}>
+//       {cards.map((card) => (
+//         <li className={css.card} key={card.id}>
+//           <ImageCard card={card} onClick={onCardClick} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
 
 
